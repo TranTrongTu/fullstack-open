a7da5be25002612f202cfe3505a8c97a82a76bc8
Trong Tu <24122024@student.hcmus.edu.vn>
2026-05-24 22:08:04 +0700

    ex 1.4

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,61 +1,64 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
 const Part = (props) => {
   return (
     <p>
       {props.name} {props.exercises}
     </p>
-  );
-};
+  )
+}
 
-const Content = (props) => { // trong object props có part1, part2, part 3 
-  // Nhận vào nguyên 1 Object, nên phải dùng dấu chấm (.) để lấy đúng tên và số
-  console.log(props);
+const Content = (props) => {
+  // Accessing array elements using indices [0], [1], [2]
   return (
     <>
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
-  );
-};
+  )
+}
 
 const Total = (props) => {
+  // Summing up the exercises from each object inside the array
   return (
     <p>
-      Number of exercises{" "}
-      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </p>
-  );
-};
+  )
+}
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const course = 'Half Stack application development'
+  
+  // The data is now grouped into a single array of objects
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
+  // Passing the entire array as a single prop
   return (
     <div>
       <Header course={course} />
-
-      {/* Truyền nguyên Object xuống cho Content và Total */}
-      <Content part1={part1} part2={part2} part3={part3} />
-
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
