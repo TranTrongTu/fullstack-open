5189da4d86901c1990843b491e7e18161456e54e
Trong Tu <24122024@student.hcmus.edu.vn>
2026-05-25 18:45:42 +0700

    ex 1.5

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -31,32 +31,31 @@ const Total = (props) => {
     </p>
   )
 }
-
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  // The data is now grouped into a single array of objects
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
+  const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
     }
-  ]
 
-  // Passing the entire array as a single prop
+    // Passing the entire array as a single prop
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
