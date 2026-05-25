8581c0343012e18116479c2638b7ea8b34dd963b
Trong Tu <24122024@student.hcmus.edu.vn>
2026-05-25 21:35:39 +0700

    learn UseState in React

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,31 +1,17 @@
-const Hello = (props) => {
-  // 1. Hàm hỗ trợ nằm gọn trong bụng Hello
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age // hàm con tự động được truy cập vào mọi biến của cha 
-  }
-
-  // 2. Hello BẮT BUỘC phải return ra giao diện
-  // Ở đây Hello mới có quyền xài props.name và hàm bornYear()
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
 
-  // 3. Nhiệm vụ duy nhất của App là truyền dữ liệu (props) xuống cho Hello
+  const [ counter, setCounter ] = useState(0)
+
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
-    <div>
-      <Hello name={name} age={age} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
