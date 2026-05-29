f86148858f68be0724a83d96cbe20ca9ae58012d
Trong Tu <24122024@student.hcmus.edu.vn>
2026-05-29 11:42:09 +0700

    must create new list to get a new address to re render

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,25 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([]) // list stores L, R, L
 
 
-  const handleClick = () => {
-    console.log('clicked')
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R')) // must use concat to create new a list -> new address
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <div>{counter}</div>
-
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-            <button onClick={() => setCounter(0)}> 
-        zero
-      </button>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      {/*join for rendering: L R L instead of render whole list*/}
+      <p>{allClicks.join(' ')}</p> 
     </div>
   )
 }
-
 export default App
