4aaa0eaf757d4a386d83ee3ca3453b20ed8c5805
Trong Tu <24122024@student.hcmus.edu.vn>
2026-05-30 23:44:06 +0700

    ex 1.12

--- part01/courseinfo/src/App.jsx
+++ part01/courseinfo/src/App.jsx
@@ -1,48 +1,37 @@
-import { useState } from 'react';
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  // variable selected stores the index of currently displayed anecdotes
+  const [selected, setSelected] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  // Random Event handler function: generate a random number to show new anecdotes
+  const handleRandomClick = () => {
+   
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    
+    setSelected(randomIndex) // update the currently displayed anecdotes
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-debugger
   return (
     <div>
-      {left}
-
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      {/* Display anecdotes*/}
+      <p>{anecdotes[selected]}</p>
+      
+      {/*Add new button and click to change current anecdotes*/}
+      <button onClick={handleRandomClick}>next anecdote</button>
     </div>
   )
 }
+
 export default App
