55f8e0da9ac2694d22bf3a127e0b5baba95adeee
Trong Tu <24122024@student.hcmus.edu.vn>
2026-06-03 20:36:25 +0700

    render list using map function

--- part02/src/App.jsx
+++ part02/src/App.jsx
@@ -1,7 +1,12 @@
-const App = () => {
+const App = (props) => {
+  const { notes } = props
+
   return (
     <div>
-      <h1>Hello Part 2</h1>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
     </div>
   )
 }
